68e46ab04a34bdc9205b94408a69b6d2f555dc88
ccztmn <106480579+ccztmn@users.noreply.github.com>
2024-11-18 18:00:45 +0800

    primary C

---
+++ 012switch_case.c
@@ -0,0 +1,53 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int type;
+//	scanf_s("%d", &type);
+//	
+//	switch (type) {
+//	case 1:
+//		printf("hello!");
+//		break;
+//	case 2:
+//		printf("good morning!");
+//		break;
+//	case 3:
+//		printf("good afternoon!");
+//		break;
+//	case 4:
+//		printf("good night!");
+//		break;
+//	default:
+//		printf("thank u!");
+//		break;
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int type;
+//	scanf_s("%d", &type);
+//	
+//	switch (type) {
+//	case 1:
+//	case 2:
+//		printf("good morning!");
+//		break;
+//	case 3:
+//		printf("good afternoon!");
+//	case 4:
+//		printf("good night!");
+//		break;
+//	default:
+//		printf("thank u!");
+//		break;
+//	}
+//
+//	return 0;
+//}
